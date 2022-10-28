5dda236269feb50bc2b686d93877917950fe26d8
Jeon Minji <mjjeon2645@gmail.com>
2022-10-28 15:29:58 +0900

    특정 장소(덕수궁) 좌표에 대한 마커 1개 노출 및 단위 테스트 1차 작성

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,6 +1,12 @@
+import { useEffect } from 'react';
 import Map from '../components/Map';
+import KakaoMap from '../modules/KakaoMapModule';
 
 export default function MapPage() {
+  useEffect(() => {
+    KakaoMap();
+  }, []);
+
   return (
     <Map />
   );
