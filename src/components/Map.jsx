45618d300a6ed57e4647aa770b6eb806a28ce1aa
Jeon Minji <mjjeon2645@gmail.com>
2022-10-29 23:02:41 +0900

    임시저장

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,25 +11,52 @@ export default function Map() {
     mapStore.fetchAllPositions();
   }, []);
 
+  // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
+  function makeOverListener(map, marker, infowindow) {
+    return () => {
+      infowindow.open(map, marker);
+    };
+  }
+
+  // 인포윈도우를 닫는 클로저를 만드는 함수입니다
+  function makeOutListener(infowindow) {
+    return () => {
+      infowindow.close();
+    };
+  }
+
   useEffect(() => {
     const container = document.getElementById('myMap');
     const options = {
       center: new kakao.maps.LatLng(37.565804, 126.975146),
-      level: 10,
+      level: 5,
     };
 
     const map = new kakao.maps.Map(container, options);
 
     const { positions } = mapStore;
 
-    for (let i = 0; i < positions.length; i += 1) {
-      const latLng = new kakao.maps.LatLng(positions[i].latitude, positions[i].longitude);
-
+    positions.forEach((value) => {
       const marker = new kakao.maps.Marker({
         map,
-        position: latLng,
+        position: new kakao.maps.LatLng(value.latitude, value.longitude),
       });
-    }
+      const infowindow = new kakao.maps.InfoWindow({
+        content: value.title,
+      });
+
+      kakao.maps.event.addListener(
+        marker,
+        'mouseover',
+        makeOverListener(map, marker, infowindow),
+      );
+
+      kakao.maps.event.addListener(
+        marker,
+        'mouseout',
+        makeOutListener(infowindow),
+      );
+    });
   }, []);
 
   return (
