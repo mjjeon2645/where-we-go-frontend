d83e2d8ed476c1a1cc71790808ec4a7ff7dc06fd
Jeon Minji <mjjeon2645@gmail.com>
2022-11-05 14:34:48 +0900

    필터로직 일부 추가(예외상황 발생 시 alert 메시지 발생)

--- src/stores/MapStore.test.js
+++ src/stores/MapStore.test.js
@@ -10,45 +10,6 @@ describe('MapStore', () => {
     mapStore = new MapStore();
   });
 
-  // context('There is 5 elements array of positions in database', () => {
-  //   it('loads an array of positions of five places', () => {
-  //     mapStore.fetchAllPositions();
-
-  //     expect(mapStore.positions).toStrictEqual([
-  //       {
-  //         id: 0,
-  //         title: '덕수궁',
-  //         latitude: 37.565804,
-  //         longitude: 126.975146,
-  //       },
-  //       {
-  //         id: 1,
-  //         title: '국립중앙박물관',
-  //         latitude: 37.523850,
-  //         longitude: 126.980470,
-  //       },
-  //       {
-  //         id: 2,
-  //         title: '서울숲 공원',
-  //         latitude: 37.544387,
-  //         longitude: 127.037442,
-  //       },
-  //       {
-  //         id: 3,
-  //         title: '양양 쏠비치',
-  //         latitude: 38.086867,
-  //         longitude: 128.665343,
-  //       },
-  //       {
-  //         id: 4,
-  //         title: '과천 서울랜드',
-  //         latitude: 37.434156,
-  //         longitude: 127.020126,
-  //       },
-  //     ]);
-  //   });
-  // });
-
   context('User clicks a marker its id is 4', () => {
     it('loads pop-up about information of the cliked place', async () => {
       mapStore.fetchSelectedPlaceInformation(4);
