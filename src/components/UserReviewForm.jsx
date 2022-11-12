aebe50e9312a4bf16a9abe81c253fead445d66fd
Jeon Minji <mjjeon2645@gmail.com>
2022-11-12 13:50:41 +0900

    사용자 평점 화면 내 요소 생성, react-datepicker 라이브러리로 방문일자 추가 가능

--- src/components/UserReviewForm.jsx
+++ src/components/UserReviewForm.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
+import { ko } from 'date-fns/esm/locale';
 import DatePicker from 'react-datepicker';
+import { addDays } from 'date-fns';
 
 export default function UserReviewForm() {
   const [startDate, setStartDate] = useState(new Date());
@@ -10,25 +12,45 @@ export default function UserReviewForm() {
   };
 
   const handleUserReviewBodyChange = (event) => {
-    const { value } = event.target;
-    console.log(value);
+    //
   };
 
   return (
-    <form onSubmit={handleUserReviewSubmit}>
-      <label htmlFor="input-date">방문일</label>
-      <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
-
-      <p>평점</p>
-      <label htmlFor="input-review">한 줄 리뷰</label>
-      <textarea
-        id="input-review"
-        placeholder="최소 10자 이상 적어주세요"
-        onChange={handleUserReviewBodyChange}
-      />
-      <p>/ 50자</p>
-      <button type="submit">등록하기</button>
+    <div>
+      <form onSubmit={handleUserReviewSubmit}>
+        <div>
+          <p>방문일</p>
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            locale={ko}
+            maxDate={addDays(new Date(), 0)}
+            dateFormat="yyyy-MM-dd"
+          />
+        </div>
+        <div>
+          <label htmlFor="rate-select">평점</label>
+          <select id="rate-select">
+            <option selected>⭐️⭐️⭐️⭐️⭐️</option>
+            <option>⭐️⭐️⭐️⭐️</option>
+            <option>⭐️⭐️⭐️</option>
+            <option>⭐️⭐️</option>
+            <option>⭐️</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="input-review">한 줄 리뷰</label>
+          <textarea
+            id="input-review"
+            placeholder="최소 10자 이상 적어주세요"
+            onChange={handleUserReviewBodyChange}
+          />
+          <p>/ 50자</p>
+        </div>
+        <button type="submit">등록하기</button>
+      </form>
       <p>욕설이나 악의적 리뷰는 관리자에 의해 가림처리 될 수 있습니다.</p>
-    </form>
+    </div>
+
   );
 }
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,21 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import UserReviewForm from '../components/UserReviewForm';
-import useUserReviewStore from '../hooks/useUserReviewStore';
-
-// CSS Modules, react-datepicker-cssmodules.css
-// import 'react-datepicker/dist/react-datepicker-cssmodules.css';
-
 export default function MyPage() {
-  const userReviewStore = useUserReviewStore();
-
-  useEffect(() => {
-    userReviewStore.fetchUserReviews(0);
-  }, []);
-
   return (
     <div>
       My Page
-      <UserReviewForm />
     </div>
   );
 }
